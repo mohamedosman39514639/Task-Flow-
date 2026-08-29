6e5b637a01e047a4287bb6ed573bb45a25a24485
mothman <mothman@sirius-its.com>
2026-08-29 14:00:46 +0300

    test: simulate CI failure

---
+++ src/test/java/com/app/taskflow/controllerTest/StatusControllerTest.java
@@ -0,0 +1,27 @@
+package com.app.taskflow.controllerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class StatusControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnApplicationStatus() throws Exception {
+
+        mockMvc.perform(get("/status/test-get-status"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Application is stopped"));
+    }
+}
